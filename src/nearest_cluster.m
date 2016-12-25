03d77d607b48f07d47496d7fba5129b31dfebe23
Meng Kiang Seah <mengkiang92@gmail.com>
2016-12-25 18:30:27 +0800

    Function to determine nearest centroid to test point.

---
+++ src/nearest_cluster.m
@@ -0,0 +1,26 @@
+function [ label ] = nearest_cluster( classes, centroids, testpoint, distance_type )
+% NEAREST_CLUSTER Using pdist(), the function selects which row of
+% centroids is closest to testpoint, and assigns it the same class. 
+%   classes is vector of class number relative to centroid
+%   centroids is a list of row vectors, each being a centroid
+%   testpoint is a vector for the test
+%   distance type is either: squaredeuclidean, cityblock, cosine, or correlation
+
+    % Confirm distance type
+    if strcmp(distance_type, 'sqeuclidean')
+        distance_type = 'euclidean';
+    end
+    % Acquire distances
+    distances = pdist([testpoint; centroids], deblank(char(distance_type)));
+    k = size(centroids, 1);
+    
+    % Element 1, 2, and 3 of distances are: 2-1, 3-1, 4-1 distances
+    % In other words the three distances we want. 
+    important_distances = distances(1:k);
+    
+    % Acquire minimum
+    [~, index] = min(important_distances);
+    % Map closest centroid to class
+    label = classes(index);
+end
+
