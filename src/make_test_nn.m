4f5029246a581f14c1149edfa30500e4435a146e
Michael Hart <michael.hart1994@gmail.com>
2016-12-28 21:54:02 +0000

    Changed calculation of ratios to remove 100 factor

--- src/make_test_nn.m
+++ src/make_test_nn.m
@@ -1,11 +1,11 @@
-function [ acc, t_train, t_test ] = make_test_nn( training, ...
-                                                  l_training, ...
-                                                  test, ...
-                                                  l_test, ...
-                                                  validation, ...
-                                                  l_validation, ...
-                                                  n_hidden, ...
-                                                  train_alg)
+function [ acc, t_train, t_test, err ] = make_test_nn( training, ...
+                                                       l_training, ...
+                                                       test, ...
+                                                       l_test, ...
+                                                       validation, ...
+                                                       l_validation, ...
+                                                       n_hidden, ...
+                                                       train_alg)
 %MAKE_TEST_NN Generate, train, and test neural network with params
 %   First six parameters are training, test, and validation data and labels
 %   n_hidden is the number of neurons to use in the hidden layer
@@ -20,7 +20,7 @@ inputs = vertcat(training, validation)';
 targets = vertcat(l_training, l_validation)';
 Na = length(training);
 Nb = length(validation);
-N = (Na/(Na+Nb)) * 100;
+N = (Na/(Na+Nb));
 
 % Create a Fitting Network
 % Possible training algorithms are as follows:
@@ -33,10 +33,13 @@ net = fitnet(n_hidden, train_alg);
 % Disable the god damn UI window
 net.trainParam.showWindow=0;
 
+% Set divide function to contiguous
+net.divideFcn = 'divideblock';
+
 % Set up Division of Data for Training, Validation, Testing
-net.divideParam.trainRatio = N/100;
-net.divideParam.valRatio = (100-N)/100;
-net.divideParam.testRatio = 0/100;
+net.divideParam.trainRatio = N;
+net.divideParam.valRatio = 1-N;
+net.divideParam.testRatio = 0;
 
 % TODO Set up number of layers to use
 
@@ -58,6 +61,10 @@ t_test = toc();
 % Compare targets to outputs
 correct = round(outputs) == targets;
 acc = sum(correct) / length(targets);
+
+% Now get squared error sum as error
+error = gsubtract(outputs, targets);
+err = error .^ 2;
 
 end
 
