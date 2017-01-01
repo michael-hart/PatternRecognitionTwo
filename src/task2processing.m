7e46c982a7d06de66843861d27d2196d1ac431c2
Meng Kiang Seah <mengkiang92@gmail.com>
2017-01-01 23:49:39 +0800

    Modified to produce and save the data.

--- src/task2processing.m
+++ src/task2processing.m
@@ -16,3 +16,26 @@ clear;
 res_path = get_res_path();
 load(strjoin({res_path 'kthreeresults.mat'}, filesep));
 load(strjoin({res_path 'ktenresults.mat'}, filesep));
+
+k_confusion_plotting(l_test, answers_k3_a, '../pic/kmeans/k3normal', 3);
+k_confusion_plotting(l_test, answers_k3_b, '../pic/kmeans/k3l2', 3);
+k_confusion_plotting(l_test, answers_k3_c, '../pic/kmeans/k3normalmaha', 3);
+k_confusion_plotting(l_test, answers_k3_d, '../pic/kmeans/k3l2maha', 3);
+k_confusion_plotting(l_test, answers_k3_e, '../pic/kmeans/k3normalmahac1', 3);
+k_confusion_plotting(l_test, answers_k3_f, '../pic/kmeans/k3l2mahac1', 3);
+k_confusion_plotting(l_test, answers_k3_g, '../pic/kmeans/k3normalmahac2', 3);
+k_confusion_plotting(l_test, answers_k3_h, '../pic/kmeans/k3l2mahac2', 3);
+k_confusion_plotting(l_test, answers_k3_i, '../pic/kmeans/k3normalmahac2', 3);
+k_confusion_plotting(l_test, answers_k3_j, '../pic/kmeans/k3l2mahac3', 3);
+
+k_confusion_plotting(l_test, answers_k10_a, '../pic/kmeans/k10normal', 3);
+k_confusion_plotting(l_test, answers_k10_b, '../pic/kmeans/k10l2', 3);
+k_confusion_plotting(l_test, answers_k10_c, '../pic/kmeans/k10normalmaha', 3);
+k_confusion_plotting(l_test, answers_k10_d, '../pic/kmeans/k10l2maha', 3);
+k_confusion_plotting(l_test, answers_k10_e, '../pic/kmeans/k10normalmahac1', 3);
+k_confusion_plotting(l_test, answers_k10_f, '../pic/kmeans/k10l2mahac1', 3);
+k_confusion_plotting(l_test, answers_k10_g, '../pic/kmeans/k10normalmahac2', 3);
+k_confusion_plotting(l_test, answers_k10_h, '../pic/kmeans/k10l2mahac2', 3);
+k_confusion_plotting(l_test, answers_k10_i, '../pic/kmeans/k10normalmahac2', 3);
+k_confusion_plotting(l_test, answers_k10_j, '../pic/kmeans/k10l2mahac3', 3);
+close all;
